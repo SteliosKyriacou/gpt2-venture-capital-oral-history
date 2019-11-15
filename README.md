1cc8dd77d3a9af17fbb10d7c5799b12cc760b778
Unknown <stelios.kyriacou@gmail.com>
2019-11-15 07:33:21 -0800

    Use

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# gpt2-venture-capital-oral-history
+# Just for fun: podt number 1 :)
+	
+Reading about the recent release of openai's [GPT-2 model](https://openai.com/blog/gpt-2-1-5b-release/) and having recently read through Berkeley's ["Venture Capitalists Oral History Project"](https://bancroft.berkeley.edu/ROHO/projects/vc/transcripts.html)
+
+
+## Finetuned model available here
+https://drive.google.com/file/d/1-7fm-M1ESOBsZva2YkKcgDm5o0gFMfkN/view?usp=sharing
---
+++ use.py
@@ -0,0 +1,22 @@
+import gpt_2_simple as gpt2
+from datetime import datetime
+
+sess = gpt2.start_tf_sess()
+gpt2.load_gpt2(sess, run_name='vc')
+
+while True:
+    raw_text = input("Model prompt >>> ")
+    while not raw_text:
+        print('Prompt should not be empty!')
+        raw_text = input("Model prompt >>> ")
+
+    gpt2.generate(sess,
+              length=250,
+              temperature=0.7,
+              prefix=raw_text,
+            
+            #   prefix='Interviewer: Whats your attitude towards risk as a ventrure capitalist?',
+              nsamples=5,
+              batch_size=5,
+              run_name='vc'
+              )
